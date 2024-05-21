22cebf4088a5707d014a4d0145436b2dd9a381c5
taldr27 <diegogarciaq98@gmail.com>
2024-05-21 13:45:54 -0500

    Update destroy correctly

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 from .serializers import ProductSerializer, ProductModel, ProductUpdateSerializer
 from cloudinary.uploader import upload
@@ -19,6 +19,16 @@ class ProductUpdateView(generics.UpdateAPIView):
 class ProductDeleteView(generics.DestroyAPIView):
     queryset = ProductModel.objects.all()
     serializer_class = ProductSerializer
+    
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            instance.status = False
+            instance.save()
+            
+            return Response({'message': 'Product deleted!'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class ProductUploadImageView(generics.GenericAPIView):
     queryset = ProductModel.objects.all()
@@ -29,7 +39,7 @@ class ProductUploadImageView(generics.GenericAPIView):
             image_file = request.FILES.get('image')
             
             if not image_file:
-                return Response({'error': 'No image uploaded!'}, status=400)
+                return Response({'error': 'No image uploaded!'}, status=status.HTTP_400_BAD_REQUEST)
             
             print(image_file)
 
@@ -38,8 +48,8 @@ class ProductUploadImageView(generics.GenericAPIView):
             image_path = f'{uploaded_image["resource_type"]}/{uploaded_image["type"]}/V{uploaded_image["version"]}{file_url_name}'
             pprint(uploaded_image)
             
-            return Response({'message': 'Image uploaded!', 'image_url': uploaded_image['url'], 'short_url': image_path})
+            return Response({'message': 'Image uploaded!', 'image_url': uploaded_image['url'], 'short_url': image_path}, status=status.HTTP_201_CREATED)
         
         except Exception as e:
-            return Response({'error': str(e)}, status=500)
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
